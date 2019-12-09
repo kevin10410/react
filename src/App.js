d759ee42a73da80b3176e597e037836a945b080d
kevin10410 <kevin10410@gmail.com>
2019-12-09 23:31:44 +0800

    refactor(App): refactor props with state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import Person from './Components/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Kevin', age: 20 },
+      { name: 'OTree', age: 18 },
+    ],
+  };
+
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
-        <Person name="Kevin" age="20"/>
-        <Person name="OTree" age="18"/>
+        <Person
+          name = { this.state.persons[0].name}
+          age  = { this.state.persons[0].age}
+        />
+        <Person
+          name = { this.state.persons[1].name}
+          age  = { this.state.persons[1].age}
+        />
       </div>
     )
   }
