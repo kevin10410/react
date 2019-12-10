ae8df20502ed90dbf08dfa5a555d1c80e612b77e
kevin10410 <kevin10410@gmail.com>
2019-12-10 22:37:37 +0800

    refactor(App): add code using react hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,45 @@
-import React, { Component } from 'react';
+import React, {
+  // useState,
+  Component,
+} from 'react';
 import Person from './Components/Person';
 import './App.css';
+
+// function App () {
+//   const [ personsState, setPersonsState ] = useState({
+//     persons: [
+//       { name: 'Kevin', age: 20 },
+//       { name: 'OTree', age: 18 },
+//     ],
+//   });
+
+//   const switchNameHandler = () => {
+//     setPersonsState({
+//       persons: [
+//         { name: 'OTree', age: 18 },
+//         { name: 'Kevin', age: 20 },
+//       ],
+//     });
+//   };
+
+//   return (
+//     <div className="App">
+//         <h1>React App</h1>
+//         <Person
+//           name = { personsState.persons[0].name}
+//           age  = { personsState.persons[0].age}
+//         />
+//         <Person
+//           name = { personsState.persons[1].name}
+//           age  = { personsState.persons[1].age}
+//         />
+//         <button
+//           onClick={ switchNameHandler }
+//         >Switch Name
+//         </button>
+//       </div>
+//   )
+// }
 
 class App extends Component {
   state = {
