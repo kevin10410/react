ac1c5dcc1c5831d2bd5c1e5007195a7bcedaf1d0
kevin10410 <kevin10410@gmail.com>
2020-01-01 22:32:20 +0800

    refactor(Person): implement dynamic bind classes

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './Person.css';
 
 const Person = (props) => {
+
+  const pClasses = ['Person'];
+  props.age > 18 && pClasses.push('red');
+  props.age > 18 && pClasses.push('bold');
+
   return (
     <div>
-      <p className="Person"
+      <p className={ pClasses.join(' ') }
         onClick = { props.click }
       >I'm { props.name } and I'm { props.age } years old</p>
       <input type="text"
