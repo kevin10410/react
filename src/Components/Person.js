58203cee2bc590260712c8cf20b81bc5edb883b3
kevin10410 <kevin10410@gmail.com>
2020-01-02 21:58:19 +0800

    style(Person): refactor css with styled component

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import './Person.scss';
+import styled from 'styled-components';
 
 const Person = (props) => {
 
-  const pClasses = ['Person'];
-  props.age > 18 && pClasses.push('Person__warning');
+  const DivPerson = styled.div`
+    width: 60%;
+    margin: 20px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+  `;
+
+  const PPersonName = styled.p`
+    color: ${props.age > 18 ? 'red' : 'blue'};
+    font-weight: ${props.age > 18 ? 'bold' : 'normal'};
+  `;
 
   return (
-    <div>
-      <p className={ pClasses.join(' ') }
+    <DivPerson>
+      <PPersonName
         onClick = { props.click }
-      >I'm { props.name } and I'm { props.age } years old</p>
+      >I'm { props.name } and I'm { props.age } years old
+      </PPersonName>
       <input type="text"
         value = { props.name }
         onChange = { props.change }
       ></input>
-    </div>
+    </DivPerson>
   )
 };
 
