7296037e2d6de6f1a0fccb4df24040d388a26084
kevin10410 <kevin10410@gmail.com>
2020-01-04 23:02:32 +0800

    fix(Person): fix styled component input blur after change issue

--- src/Components/Persons/Person.js
+++ src/Components/Persons/Person.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
+const DivPerson = styled.div`
+  width: 60%;
+  margin: 20px auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 16px;
+  text-align: center;
+`;
+
+const PPersonName = styled.p`
+  color: ${ props => props.age > 18 ? 'red' : 'blue'};
+  fontWeight: ${ props => props.age > 18 ? 'bold' : 'normal'};
+`;
+
 const Person = (props) => {
-
-  const DivPerson = styled.div`
-    width: 60%;
-    margin: 20px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-  `;
-
-  const PPersonName = styled.p`
-    color: ${props.age > 18 ? 'red' : 'blue'};
-    font-weight: ${props.age > 18 ? 'bold' : 'normal'};
-  `;
+  const personNameStyleObj = {
+    color: `${props.age > 18 ? 'red' : 'blue'}`,
+    fontWeight: `${props.age > 18 ? 'bold' : 'normal'}`,
+  };
 
   return (
     <DivPerson>
       <PPersonName
+        style = {personNameStyleObj}
         onClick = { props.click }
       >I'm { props.name } and I'm { props.age } years old
       </PPersonName>
