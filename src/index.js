ba4a991dae71e3bba277c8e3396f8cfa48a0f6ab
kevin10410 <kevin10410@gmail.com>
2020-01-05 11:37:41 +0800

    chore(package): create axios-mocker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Containers/App';
 import * as serviceWorker from './serviceWorker';
+import './mock/axios-mocker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
---
+++ src/mock/axios-mocker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+// eslint-disable-next-line no-unused-vars
+const mock = new MockAdapter(axios);
